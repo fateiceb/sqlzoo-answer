bac65171bbe31a250c578b5062c68869f8288a1b
fateiceb <993661862@qq.com>
2021-09-17 15:46:42 +0800

    add quzi

---
+++ 0 SELECT basics/quiz.sql
@@ -0,0 +1,20 @@
+--1. Select the code which produces this table 
+--ans: 3
+
+--2. Pick the result you would obtain from this code 
+--ans: 5 
+
+--3. Select the code which shows the countries that end in A or L 
+--ans: 5
+
+--4. Pick the result from the query 
+--ans: 3
+
+--5. Pick the result you would obtain from this code
+--ans：3
+
+--6. Select the code that would show the countries with an area larger than 50000 and a population smaller than 10000000 
+--ans: 3
+
+--7. Select the code that shows the population density of China, Australia, Nigeria and France 
+--ans: 3
